ff37b919c09cf428cbc4d2c33751d920c8bf6cd9
Farrux Temurov <tfarrux70@gmail.com>
2025-01-17 10:00:05 +0500

    implemented getDispatcher concept

--- LoginServletApp/src/main/java/com/LoginServlet.java
+++ LoginServletApp/src/main/java/com/LoginServlet.java
@@ -3,6 +3,7 @@ package com;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,11 +25,17 @@ public class LoginServlet extends HttpServlet {
 		// request.getParameter("fieldName") which helps to receive the value as string
 		String emailid = request.getParameter("emailid");
 		String password = request.getParameter("password");
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+
 		if (emailid.equals("test@gmail.com") && password.equals("test@")) {
 			pw.println("Successful login! with GET Method");
+			rd1.forward(request, response); // target page output.
 		} else {
 			pw.println("Failure try once again! with GET Method");
+			rd2.include(request, response); // error message + login.html page code.
 		}
+		response.setContentType("text/html");// set response as html format.
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -37,11 +44,17 @@ public class LoginServlet extends HttpServlet {
 		// request.getParameter("fieldName") which help to receive the value as string
 		String emailid = request.getParameter("emailid");
 		String password = request.getParameter("password");
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+
 		if (emailid.equals("test@gmail.com") && password.equals("test@")) {
 			pw.println("Successful login! with POST Method");
+			rd1.forward(request, response); // target page output.
 		} else {
 			pw.println("Failure try once again! with POST Method");
+			rd2.include(request, response); // error message + login.html page code.
 		}
+		response.setContentType("text/html");// set response as html format.
 	}
 
 }
