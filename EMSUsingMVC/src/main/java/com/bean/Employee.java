b59edad7bb0231c1b9eed3afe3d8849275a549d9
Farrux Temurov <tfarrux70@gmail.com>
2025-01-29 20:49:24 +0500

    EMSUsingMVC project setup completed

---
+++ EMSUsingMVC/src/main/java/com/bean/Employee.java
@@ -0,0 +1,43 @@
+package com.bean;
+
+import java.time.LocalDate;
+
+public class Employee {
+	private int id;
+	private String name;
+	private float salary;
+	private LocalDate doj;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	public LocalDate getDoj() {
+		return doj;
+	}
+
+	public void setDoj(LocalDate doj) {
+		this.doj = doj;
+	}
+
+}
