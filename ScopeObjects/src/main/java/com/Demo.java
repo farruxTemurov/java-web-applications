83013ba0c2d254f595c4d2d4bbfb1eda47dae484
Farrux Temurov <tfarrux70@gmail.com>
2025-01-30 21:53:27 +0500

    implemented sendRedirect, set/getAttribute methods

--- ScopeObjects/src/main/java/com/Demo.java
+++ ScopeObjects/src/main/java/com/Demo.java
@@ -1,12 +1,19 @@
 package com;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+/**
+ * Servlet implementation class Demo
+ */
 @WebServlet("/Demo")
 public class Demo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -16,16 +23,47 @@ public class Demo extends HttpServlet {
 		// TODO Auto-generated constructor stub
 	}
 
+	int id = 100;
+	String name = "Steven";
+	float salary = 14000.50f; // instance
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		PrintWriter pw = response.getWriter();
+		pw.println("<br/> In Servlet Page ");
+		pw.println("<br/> id is " + id);
+		pw.println("<br/> name is " + name);
+		pw.println("<br/> salary is " + salary);
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// 1st approach
+//		RequestDispatcher rd = request.getRequestDispatcher("welcome.jsp");
+//		request.setAttribute("eid", id);
+//		request.setAttribute("ename", name);
+//		request.setAttribute("esalary", salary);
+//		rd.include(request, response);					// source page code + target page code merge 
+
+		// 2nd approach
+//		RequestDispatcher rd = request.getRequestDispatcher("welcome.jsp");
+//		request.setAttribute("eid", id);
+//		request.setAttribute("ename", name);
+//		request.setAttribute("esalary", salary);
+//		rd.forward(request, response);					// we can see only target page output. 
+
+		// 3rd approach
+//		request.setAttribute("eid", id);
+//		request.setAttribute("ename", name);
+//		request.setAttribute("esalary", salary);
+//		response.sendRedirect("welcome.jsp");		// we get output of target page as well as it 
+		// generate new request
+
+		// 4th approach
+		HttpSession hs = request.getSession();
+		hs.setAttribute("eid", id);
+		hs.setAttribute("ename", name);
+		hs.setAttribute("esalary", salary);
+		response.sendRedirect("welcome.jsp");
+
+		response.setContentType("text/html");
 	}
 
 }
--- ScopeObjects/src/main/webapp/welcome.jsp
+++ ScopeObjects/src/main/webapp/welcome.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,13 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<%
+	int id = (int) session.getAttribute("eid"); //typecasting is not necessary when displaying. Here it is for practice purposes only
+	String name = (String) session.getAttribute("ename");
+	float salary = (float) session.getAttribute("esalary");
+	out.println("<br/> Id is " + id);
+	out.println("<br/> Name is " + name);
+	out.println("<br/> Salary is " + salary);
+	%>
 </body>
 </html>
