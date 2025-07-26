85280f5ef0425e0b291e5c332dac79359f48876b
Farrux Temurov <tfarrux70@gmail.com>
2025-07-26 22:48:22 +0500

    Updated SignInController.java

--- LoginMVCApp/src/main/java/com/controller/SignInController.java
+++ LoginMVCApp/src/main/java/com/controller/SignInController.java
@@ -22,12 +22,10 @@ public class SignInController extends HttpServlet {
 
 	public SignInController() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
