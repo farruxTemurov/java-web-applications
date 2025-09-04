6dbd2e813babbbfe098ee2f575da8bab33b44044
Farrux Temurov <tfarrux70@gmail.com>
2025-09-04 23:39:49 +0500

    Updated Home.java

--- LoginServletApp/src/main/java/com/Home.java
+++ LoginServletApp/src/main/java/com/Home.java
@@ -15,7 +15,6 @@ public class Home extends HttpServlet {
 
 	public Home() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
