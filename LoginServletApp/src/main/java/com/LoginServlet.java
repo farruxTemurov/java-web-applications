40b78816ef3aa5f05e1a9a7d7c8a3e014d4a0319
Farrux Temurov <tfarrux70@gmail.com>
2025-09-06 22:52:52 +0500

    Updated LoginServlet.java

--- LoginServletApp/src/main/java/com/LoginServlet.java
+++ LoginServletApp/src/main/java/com/LoginServlet.java
@@ -14,11 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public LoginServlet() {
-		super();
-
-	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();// it helps to display the output on browser
